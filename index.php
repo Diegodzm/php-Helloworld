6a76024c450d98886eb2d769a5ac81420f9131f6
DiegoDiaz <diegodiazmed93@gmail.com>
2024-07-23 18:31:26 -0400

    1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  $name="diego"
+ 
+?>
+
+<h1>
+    <?= "primer php";?>
+
+</h1>
+<style>
+    :root{
+        color-scheme: light dark;
+    }
+    body {
+        display:grid;
+        place-content: center;
+    }
+</style>
